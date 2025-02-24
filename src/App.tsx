de72d54712c68a2452c348c33f244c24ead8a169
KVECKO <samo.knieza@gmail.com>
2025-02-24 16:48:49 +0100

    first test for gitpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-
-import './App.css'
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <>
-      
+      <Navbar></Navbar>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
